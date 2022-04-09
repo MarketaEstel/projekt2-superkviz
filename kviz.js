67f0e3a8c4ffe9723cd03842624f5b360481583d
MarketEstel <estelka.m@gmail.com>
2022-04-09 11:53:54 +0200

    vytvořeny prvky pro html

--- kviz.js
+++ kviz.js
@@ -1,6 +1,51 @@
-// Nejdříve si vytvoř objekt, který bude držet voje super otázky :-)
+let kviz = document.createElement("div");
+kviz.classList.add(".kviz")
 
-const otazky = [];
+let poleOdpoved = [
+{odpoved: "Kočičák"},
+{odpoved: "Mončičák"},
+{odpoved: "Opičák"}
+]
+
+const otazky = [
+    {otazka: "Co je ikonická hračka z 80. let?", foto: "obrazky/moncicak.jpg", jmeno: "mončičák"},
+    {otazka: "Jaké je Matějovo nejoblíbenější ovoce?", foto: "obrazky/ovoce.jpg", jmeno: "ovoce"},
+    {otazka: "Pro úspěšné absolvování kurzu je potřeba...", foto: "obrazky/pivo.jpg", jmeno: "pivo"}
+]
+console.log(otazky)
+
+for (i=0; i < otazky.length; i++) {
+let otazka = document.createElement("h1");
+otazka.textContent = otazky.otazka;
+
+let otazkaPoradi = document.createElement("h3")
+otazkaPoradi.classList.add("#poradi")
+
+
+let obrazekOtazka = document.createElement("img");
+obrazekOtazka.classList.add("#obrazek");
+obrazekOtazka.src = otazky[i].foto;
+obrazekOtazka.alt = otazky[i].jmeno;
+
+let odpoved = document.createElement("li");
+odpoved.classList.add("#li");
+odpoved.textContent = poleOdpoved;
+console.log(odpoved)
+
+let odpovedi = document.createElement("ul");
+odpovedi.classList.add("#odpovedi");
+console.log(otazka)
+
+let vyber = document.createElement("div");
+
+// vyber.appendChild(otazky)
+// vyber.appendChild(odpoved)
+
+// kviz.appendChild(vyber);
+
+}
+
+// Nejdříve si vytvoř objekt, který bude držet voje super otázky :-)
 
 // Dále budeš potřebovat další proměnné - jaké?
 
