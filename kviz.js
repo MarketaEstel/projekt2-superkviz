e82a23b70b1a25de6ce6c6a75cc7d5a1e7d905c3
MarketEstel <estelka.m@gmail.com>
2022-04-11 20:49:25 +0200

    přepsání JS 01

--- kviz.js
+++ kviz.js
@@ -1,100 +1,58 @@
-let kviz = document.querySelector("div");
-kviz.classList.add(".kviz")
-
-let poleOdpovedi = [
-{dataOdpoved: "Kočičák"},
-{dataOdpoved: "Mončičák"},
-{dataOdpoved: "Opičák"},
-{dataOdpoved: "Kokos"},
-{dataOdpoved: "Melounek"},
-{dataOdpoved: "Jahoda"},
-{dataOdpoved: "Ani jedna z možností"},
-{dataOdpoved: "Umět JavaScript"},
-{dataOdpoved: "Chodit po kurzu do hospody"}
-]
 
 const otazky = [
-    {otazka: "Co je ikonická hračka z 80. let?", foto: "obrazky/moncicak.jpg", jmeno: "mončičák"},
-    {otazka: "Jaké je Matějovo nejoblíbenější ovoce?", foto: "obrazky/ovoce.jpg", jmeno: "ovoce"},
-    {otazka: "Pro úspěšné absolvování kurzu je potřeba...", foto: "obrazky/pivo.jpg", jmeno: "pivo"}
-]
-console.log(otazky)
+    { otazka: "Co je ikonická hračka z 80. let?", foto: "obrazky/moncicak.jpg", jmeno: "mončičák" },
+    { otazka: "Jaké je Matějovo nejoblíbenější ovoce?", foto: "obrazky/ovoce.jpg", jmeno: "ovoce" },
+    { otazka: "Pro úspěšné absolvování kurzu je potřeba...", foto: "obrazky/pivo.jpg", jmeno: "pivo" }
+];
+console.log(otazky);
+//for cyklus v kterém je napsán kvíz  
 
-//for cyklus v kterém je napsán kvíz :/
-for (i=0; i < otazky.length; i++) {
-let otazka = document.createElement("div")
 
-let otazkaNadpis = document.createElement("h2");
-otazkaNadpis.textContent = "Otázka" + " " + 1
-otazkaNadpis.classList.add("#otazka")
 
-//otázka
-let otazkaPoradi = document.createElement("p")
-otazkaPoradi.classList.add("#poradi")
-otazkaPoradi.textContent = otazky[0].otazka
+    for (i = 0; i < 1; i = i + 1) {
 
-//div, který obsahuje div foto, obrázek a seznam odpovědí
-let obsah = document.createElement("div");
-obsah.classList.add(".obsah")
+    let kviz = document.querySelector("div");
+    kviz.classList.add(".kviz");
 
-//obrázek k otázce
-let obrazekOtazka = document.createElement("img");
-obrazekOtazka.classList.add("#obrazek");
-obrazekOtazka.src = otazky[0].foto;
-obrazekOtazka.alt = otazky[0].jmeno;
+        let poradi = document.querySelector("p");
+        poradi.textContent = "Pořadí" + " " + "1/3"
 
-//div, který obsahuje obrázek
-let foto = document.createElement("div")
-foto.classList.add(".foto")
+        let nadpis = document.querySelector("h2");
+        nadpis.textContent = otazky[i].otazka;
 
-//div, který obsahuje seznam odpovědí
-let moznosti = document.createElement("div")
-moznosti.classList.add("#moznosti")
+            let obsah = document.createElement("div");
+            obsah.classList.add(".obsah");
 
-//seznam, který obsahuje odpovědi
-let odpovedi = document.createElement("ul")
-odpovedi.textContent = poleOdpovedi.dataOdpoved
-odpovedi.classList.add("#odpovedi")
+                let foto = document.createElement("div");
+                foto.classList.add(".foto");
 
-//li vytvořené JS, které obsahuje jednotlivé položky odpovědí
-let odpoved = document.createElement("li");
-odpoved.classList.add("li");
-odpoved.textContent = poleOdpovedi[i].dataOdpoved;
-console.log(odpoved)
-// odpoved.onclick = priKliknuti({
-    
-// })
+                let img = document.createElement("img");
+                img.classList.add("#obrazek");
+                img.src = otazky[i].foto;
+                
+                foto.appendChild(img)
+                obsah.appendChild(foto)
+                kviz.appendChild(obsah) 
 
-odpovedi.appendChild(odpoved)
-moznosti.appendChild(odpovedi)
+                let odpoved = document.querySelector("li").dataset.dataOdpoved
+                console.log(odpoved)
+            
+    }
 
-foto.appendChild(obrazekOtazka)
+function zobrazOtazku() {}
+ 
 
-obsah.appendChild(foto)
-obsah.appendChild(moznosti)
-
-otazka.appendChild(obsah)
-otazka.appendChild(otazkaNadpis)
-otazka.appendChild(otazkaPoradi)
-
-kviz.appendChild(otazka);
-
-}
-
-// Nejdříve si vytvoř objekt, který bude držet voje super otázky :-)
-
+// Nejdříve si vytvoř objekt, který bude držet tvoje super otázky :-) 
 // Dále budeš potřebovat další proměnné - jaké?
-
-
 // Tato funkce se postará o vygenerování otázky
 // Zavoláme ji jednou na začátku a poté vždy po odpovězení
-function zobrazOtazku() {}
+function priKliknuti() { } 
 
 // Funkce se postará o obsluhu kliknutí na odpověď
 // Musíme ji navázat na kokrétní odpovědi každé otázky (to uděláme v rámci funkce zobrazOtazku())
-function klikNaOdpoved() {}
+function klikNaOdpoved() { }
 
 // Když už mám odpovězeno na vše (řídí se velikosí objektu otazky na řádku 3), tak mohu zobrazi výsledky
 // Vypočítám skóre a nageneruje nové elementy do HTML
 // Touto funkcí končí můj program (budu se rozhodovat, zda ji zavolat v rámci klikNaOdpoved())
-function zobrazVyhodnoceni() {}
+function zobrazVyhodnoceni() { }
